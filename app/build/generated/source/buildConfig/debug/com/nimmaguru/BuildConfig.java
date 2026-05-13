f31eaa6a1c235b1a2423d544c528d81a173c7c2f
meghanahdinesh <meghanahdinesh@gmail.com>
2026-05-13 19:48:52 +0530

    initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/nimmaguru/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nimmaguru;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nimmaguru";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1cXBpdmZwdWh5aXNxaGJlY2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNjYzNTYsImV4cCI6MjA5Mzc0MjM1Nn0.uEQbv4AiyeX2eFTplTOBUSr-jzg46UBOEprn8aSthIo";
+  // Field from default config.
+  public static final String SUPABASE_URL = "https://puqpivfpuhyisqhbeclp.supabase.co";
+}
